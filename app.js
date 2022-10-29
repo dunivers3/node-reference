ac375f4d509b4a6c0bca633e37b63bd7913774ba
Duncan <wangari.dun@gmail.com>
2022-10-29 19:44:45 +0300

    adds middleware logic 1(one)

--- app.js
+++ app.js
@@ -1,38 +1,21 @@
 const express = require("express")
-const {products, transformers} = require("./data")
 
 const server = express()
 
-//route 1 ==> GET /
+//req ==> middleware ==> res
+
 server.get("/", (req,res)=>{
-  res.json(transformers)
+  const method = req.method
+  const url = req.url
+  const time = new Date().getFullYear()
+  console.log(method, url, time)
+  res.status(200).send("Homepage!")
 })
 
-//route 2 ==> GET /api
-server.get("/api", (req,res)=>{
-  res.json(products)
+server.get("/about", (req,res)=>{
+  res.status(200).send("About page!")
 })
 
-//route 3 ==> GET /api/products
-server.get("/api/products", (req,res)=>{
-  const newProd = products.map(el => {
-    const {id, name, image} = el
-    return {id, name, image}
-  })
-  res.json(newProd)
-})
-
-//route 4 ==> GET /api/products/random_products
-server.get("/api/products/:prodId", (req,res)=>{
-  console.log(req.params)
-  const {prodId} = req.params
-  const singleProd = products.find(el => el.id === Number(prodId) )
-  if(!singleProd){
-    res.status(404).send("404! Product not found. Try again later!")
-  }
-  return res.json(singleProd)
-})
-
-server.listen(2121, ()=> {
-  console.log("Server is running on port 2121, like the savage!")
+server.listen(2121, ()=>{
+  console.log("Server is listening on port 2121, just like the savage!")
 })
