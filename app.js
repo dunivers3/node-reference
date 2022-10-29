a3fc0d37378a601d3a394546d0f28e088469237a
Duncan <wangari.dun@gmail.com>
2022-10-29 20:09:14 +0300

    adds middleware logic 2 ==> function

--- app.js
+++ app.js
@@ -4,15 +4,26 @@ const server = express()
 
 //req ==> middleware ==> res
 
-server.get("/", (req,res)=>{
+let logger = (req,res,next)=>{
+  const method = req.method
+  const url = req.url
+  const time = new Date().getFullYear()
+  console.log(method, url, time)
+  next()
+}
+
+server.get("/", logger, (req,res)=>{
+  res.status(200).send("Homepage!")
+/*
   const method = req.method
   const url = req.url
   const time = new Date().getFullYear()
   console.log(method, url, time)
   res.status(200).send("Homepage!")
+*/
 })
 
-server.get("/about", (req,res)=>{
+server.get("/about",logger, (req,res)=>{
   res.status(200).send("About page!")
 })
 
