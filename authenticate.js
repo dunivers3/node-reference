d5717d62b8f4eea8d25b6b55cf02acfbe65a2ca5
Duncan <wangari.dun@gmail.com>
2022-11-09 19:19:42 +0300

    authenticate middleware update

--- authenticate.js
+++ authenticate.js
@@ -1,6 +1,11 @@
 const authorize = (req, res, next) => {
-  console.log("Authorized!")
-  next()
+  const { user } = req.query
+  if (user === "dominus") {
+    req.user = { name: "dominus", id: 29 }
+    next()
+  } else {
+    res.status(401).send("Unauthorized!")
+  }
 }
 
 module.exports = authorize
